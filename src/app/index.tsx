9ec8420f7f8381fae1b22825c15c71331c37d416
LukasPio <lucaspio.galvao@gmail.com>
2025-02-21 17:46:55 -0300

    create a starter register page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,18 +3,24 @@ import { View, Text, StyleSheet } from "react-native";
 import { Length } from "@/enums/length";
 import Input from "@/components/Input";
 import colors from "@/constants/colors";
-import { Link } from "expo-router";
+import Button from "@/components/Button";
+import TextLink from "@/components/TextLink";
 
 export default function Register() {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [name, setName] = useState("");
 
     return (
         <View style={styles.container}>
-            <Link href={"/tabs/login"}>LOGIN</Link>
-            <Input title="Email" value={email} placeholder="exemplo@gmail.com" onChangeText={setEmail} size={Length.large}/>
-            <Input title="Password" value={password} onChangeText={setPassword} size={Length.large}/>
+            <Input title="Nome" value={name} onChangeText={setName} size={Length.medium}/>
+            <Input title="Email" isEmail={true} value={email} onChangeText={setEmail} size={Length.medium}/>
+            <Input title="Senha" isPassword={true} value={password} onChangeText={setPassword} size={Length.medium}/>
+            <Input title="Confirme a senha" isPassword={true} value={passwordConfirm} onChangeText={setPasswordConfirm} size={Length.medium}/>
+            <TextLink href="/login">Já possui uma conta? Clique aqui.</TextLink>
+            <Button text="Criar conta" size={Length.medium}/>
         </View>
     )
 }
@@ -24,7 +30,9 @@ const styles = StyleSheet.create(
     container: {
         flex: 1,
         alignItems: "center",
-        backgroundColor: colors.background
+        justifyContent: "center",
+        backgroundColor: colors.background,
+        gap: 12
     }
  }
 )
