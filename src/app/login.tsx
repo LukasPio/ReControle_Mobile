93ba892a06e8a0c992722ffca7599e8620adf554
LukasPio <lucaspio.galvao@gmail.com>
2025-02-21 21:34:08 -0300

    create login page with initial logic

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Length } from "@/enums/length";
 import Input from "@/components/Input";
 import colors from "@/constants/colors";
+import Button from "@/components/Button";
+import CustomLink from "@/components/CustomLink";
+import Logo from "@/components/Logo";
+import { login } from "@/services/firebaseServices";
 
 export default function Login() {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
     return (
         <View style={styles.container}>
-            <Text>LOGIN</Text>
-            <Input title="Email" value={email} placeholder="exemplo@gmail.com" onChangeText={setEmail} size={Length.large}/>
-            <Input title="Password" value={password} onChangeText={setPassword} size={Length.large}/>
+            <Logo size={Length.large}/>
+            <Input title="Email" isEmail={true} value={email} onChangeText={setEmail} size={Length.medium}/>
+            <Input title="Senha" isPassword={true} value={password} onChangeText={setPassword} size={Length.medium}/>
+            <CustomLink href="/">Ainda não possui uma conta? Clique aqui.</CustomLink>
+            <Button text="Fazer login" onPress={() => login({email, password})} size={Length.medium}/>
         </View>
     )
 }
@@ -23,7 +29,9 @@ const styles = StyleSheet.create(
     container: {
         flex: 1,
         alignItems: "center",
-        backgroundColor: colors.background
+        backgroundColor: colors.background,
+        gap: 12,
+        paddingTop: 40 
     }
  }
 )
