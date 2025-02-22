391b3d63f35b8743ebe7b7bace7aa810ce437a14
LukasPio <lucaspio.galvao@gmail.com>
2025-02-21 21:34:24 -0300

    create register page with initial logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Length } from "@/enums/length";
 import Input from "@/components/Input";
 import colors from "@/constants/colors";
 import Button from "@/components/Button";
-import TextLink from "@/components/TextLink";
+import CustomLink from "@/components/CustomLink";
+import Logo from "@/components/Logo";
+import { createAccount } from "@/services/firebaseServices";
 
 export default function Register() {
 
@@ -15,12 +17,13 @@ export default function Register() {
 
     return (
         <View style={styles.container}>
+            <Logo size={Length.large}/>
             <Input title="Nome" value={name} onChangeText={setName} size={Length.medium}/>
             <Input title="Email" isEmail={true} value={email} onChangeText={setEmail} size={Length.medium}/>
             <Input title="Senha" isPassword={true} value={password} onChangeText={setPassword} size={Length.medium}/>
             <Input title="Confirme a senha" isPassword={true} value={passwordConfirm} onChangeText={setPasswordConfirm} size={Length.medium}/>
-            <TextLink href="/login">Já possui uma conta? Clique aqui.</TextLink>
-            <Button text="Criar conta" size={Length.medium}/>
+            <CustomLink href="/login">Já possui uma conta? Clique aqui.</CustomLink>
+            <Button text="Criar conta" onPress={() => createAccount({name, email, password, passwordConfirm})} size={Length.medium}/>
         </View>
     )
 }
@@ -30,9 +33,9 @@ const styles = StyleSheet.create(
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
         backgroundColor: colors.background,
-        gap: 12
+        gap: 12,
+        paddingTop: 40 
     }
  }
 )
