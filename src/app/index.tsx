573d574a5c3b509b6e9274621e379852d165e6e0
LukasPio <lucaspio.galvao@gmail.com>
2025-02-23 03:14:28 -0300

    update to new auth system

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,36 +6,60 @@ import colors from "@/constants/colors";
 import Button from "@/components/Button";
 import CustomLink from "@/components/CustomLink";
 import Logo from "@/components/Logo";
-import { createAccount } from "@/services/firebaseServices";
+import { registerUser } from "@/services/firebaseServices";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [name, setName] = useState("");
-
-    return (
-        <View style={styles.container}>
-            <Logo size={Length.large}/>
-            <Input title="Nome" value={name} onChangeText={setName} size={Length.medium}/>
-            <Input title="Email" isEmail={true} value={email} onChangeText={setEmail} size={Length.medium}/>
-            <Input title="Senha" isPassword={true} value={password} onChangeText={setPassword} size={Length.medium}/>
-            <Input title="Confirme a senha" isPassword={true} value={passwordConfirm} onChangeText={setPasswordConfirm} size={Length.medium}/>
-            <CustomLink href="/login">Já possui uma conta? Clique aqui.</CustomLink>
-            <Button text="Criar conta" onPress={() => createAccount({name, email, password, passwordConfirm})} size={Length.medium}/>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Logo size={Length.large} />
+      <Input
+        title="Nome"
+        value={name}
+        onChangeText={setName}
+        size={Length.medium}
+      />
+      <Input
+        title="Email"
+        isEmail={true}
+        value={email}
+        onChangeText={setEmail}
+        size={Length.medium}
+      />
+      <Input
+        title="Senha"
+        isPassword={true}
+        value={password}
+        onChangeText={setPassword}
+        size={Length.medium}
+      />
+      <Input
+        title="Confirme a senha"
+        isPassword={true}
+        value={passwordConfirm}
+        onChangeText={setPasswordConfirm}
+        size={Length.medium}
+      />
+      <CustomLink href="/login">Já possui uma conta? Clique aqui.</CustomLink>
+      <Button
+        text="Criar conta"
+        onPress={() => registerUser({ name, email, password, passwordConfirm })}
+        size={Length.medium}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create(
- {  
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: colors.background,
-        gap: 12,
-        paddingTop: 40 
-    }
- }
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.background,
+    gap: 12,
+    paddingTop: 40,
+  },
+});
--- src/app/login.tsx
+++ src/app/login.tsx
@@ -6,32 +6,45 @@ import colors from "@/constants/colors";
 import Button from "@/components/Button";
 import CustomLink from "@/components/CustomLink";
 import Logo from "@/components/Logo";
-import { login } from "@/services/firebaseServices";
+import { loginUser } from "@/services/firebaseServices";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-    return (
-        <View style={styles.container}>
-            <Logo size={Length.large}/>
-            <Input title="Email" isEmail={true} value={email} onChangeText={setEmail} size={Length.medium}/>
-            <Input title="Senha" isPassword={true} value={password} onChangeText={setPassword} size={Length.medium}/>
-            <CustomLink href="/">Ainda não possui uma conta? Clique aqui.</CustomLink>
-            <Button text="Fazer login" onPress={() => login({email, password})} size={Length.medium}/>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Logo size={Length.large} />
+      <Input
+        title="Email"
+        isEmail={true}
+        value={email}
+        onChangeText={setEmail}
+        size={Length.medium}
+      />
+      <Input
+        title="Senha"
+        isPassword={true}
+        value={password}
+        onChangeText={setPassword}
+        size={Length.medium}
+      />
+      <CustomLink href="/">Ainda não possui uma conta? Clique aqui.</CustomLink>
+      <Button
+        text="Fazer login"
+        onPress={() => loginUser({ email, password })}
+        size={Length.medium}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create(
- {  
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: colors.background,
-        gap: 12,
-        paddingTop: 40 
-    }
- }
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.background,
+    gap: 12,
+    paddingTop: 40,
+  },
+});
